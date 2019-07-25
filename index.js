583bc11cc7d55462e269610df5af61ba355ad04d
Shail Shah <anonshail@gmail.com>
2019-07-25 15:06:48 +0530

    added yt-search

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const fs = require('fs');
 const ytdl = require('ytdl-core');
+const ytSearch = require( 'yt-search' )
+
+ytSearch( 'superman theme', function ( err, r ) {
+    if ( err ) throw err
+   
+    const videos = r.videos
+    const playlists = r.playlists
+    const accounts = r.accounts
+   
+    const firstResult = videos[ 0 ]
+   
+    console.log( videos.length )
+  } )
 
 const url = 'http://www.youtube.com/watch?v=A02s8omM_hI';
-
-// ytdl(url).pipe(fs.createWriteStream('video.flv'));
   
-ytdl(url, {filter: 'audioonly'}).pipe(fs.createWriteStream('audio.mp3'))
+// ytdl(url, {filter: 'audioonly'}).pipe(fs.createWriteStream('audio.mp3'))
