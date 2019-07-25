880c389c0da8171ac751b9f8a800946dcbc5b322
Shail Shah <anonshail@gmail.com>
2019-07-25 16:43:15 +0530

    something and all

--- index.js
+++ index.js
@@ -12,23 +12,7 @@ for(let i=0; i<videoKw.length; i++){
 		let title = r.videos[0].title;
 		let url = r.videos[0].url;
 
-		ytdl(url, {filter: 'audioonly'}).pipe(fs.createWriteStream(`${title}.mp`))
+		ytdl(url, {filter: 'audioonly'}).pipe(fs.createWriteStream(`./songs/${title}.mp3`));
 
 	})
 }
-
-// ytSearch( 'superman theme', function ( err, r ) {
-//     if ( err ) throw err
-   
-//     const videos = r.videos
-//     const playlists = r.playlists
-//     const accounts = r.accounts
-   
-//     const firstResult = videos[ 0 ]
-   
-//     console.log( videos.length )
-//   } )
-
-// const url = 'http://www.youtube.com/watch?v=A02s8omM_hI';
-  
-// ytdl(url, {filter: 'audioonly'}).pipe(fs.createWriteStream('audio.mp3'))
